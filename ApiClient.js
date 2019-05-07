b5b650ae22be35cf6c9d4a22fe1523150cf517bd
vladislao <netcemetery@yandex.ru>
2019-05-07 19:17:18 +0400

    Use fetch for ApiClient

--- ApiClient.js
+++ ApiClient.js
@@ -1,70 +1,76 @@
-import superagent from "superagent";
-import {AsyncStorage} from 'react-native';
-
-
-const urls = ["/login", "/register", "/confirm/invite", "/logout"];
+import { AsyncStorage } from "react-native";
+import { stringify } from "query-string";
 
 const methods = ["get", "post", "put", "patch", "del"];
-
-
-const localStorageSupported = typeof localStorage !== "undefined";
+const urls = ["/login", "/register", "/confirm/invite", "/logout"];
 
 function formatUrl(path) {
   const adjustedPath = path[0] !== "/" ? `/${path}` : path;
 
   let apiSegment = "/api";
-
-  // некоторые запросы необходимо отлавливать на сервере приложения
-  // к отлавливаемым запросам не нужно добавлять /api, иначе они уйдут на апи сервер
   urls.forEach(url => {
     if (adjustedPath.indexOf(url) === 0) {
-      apiSegment = "";
+      apiSegment = "/auth";
     }
   });
 
-  // добавление `/api` к относительному пути, для отправки на API сервер
-  return adjustedPath.indexOf("/auth") === -1
-    ? apiSegment + adjustedPath
-    : adjustedPath;
+  const fullPath =
+    adjustedPath.indexOf("/auth") === -1
+      ? apiSegment + adjustedPath
+      : adjustedPath;
+
+  return `https://tenderplan.ru${fullPath}`;
 }
 
 export default class ApiClient {
   constructor() {
-    this.cookies = [];
-
     methods.forEach(method => {
-      this[method] = (
+      this[method] = async (
         path,
         { params, data, headers } = {},
         allowAnonymous = false
-      ) =>
-        new Promise((resolve, reject) => {
-          const formattedUrl = formatUrl(path);
-          const request = superagent[method](formattedUrl);
-          if (params) {
-            request.query(params);
+      ) => {
+        const options = {
+          method: method.toUpperCase(),
+          headers: {
+            "Content-Type": "application/json"
           }
-          _getToken = async () => {
-            const userToken = await AsyncStorage.getItem('userToken');
-            return userToken
+        };
+
+        if (!allowAnonymous) {
+          const jwt = await AsyncStorage.getItem("userToken");
+          if (jwt) {
+            options.headers.Authorization = jwt;
+          }
+        }
+
+        if (data) {
+          options.body = JSON.stringify(data);
+        }
+
+        const formattedUrl = formatUrl(path);
+        const queryString = stringify(params);
+        const url = queryString
+          ? `${formattedUrl}?${queryString}`
+          : formattedUrl;
+
+        try {
+          const res = await fetch(url, options);
+
+          if (res.ok) {
+            try {
+              return await res.json();
+            } catch (e) {
+              // text fallback
+              return await res.text();
+            }
           }
 
-          const jwt = localStorageSupported ? localStorage.getItem("token") : (
-            _getToken()
-          )
-
-          if (!allowAnonymous && jwt) {
-            request.set("Authorization", jwt);
-          }
-
-          if (data) {
-            request.send(data);
-          }
-
-          request.end((err, res = {}) => {
-            err ? reject(res.body || err) : resolve(res.body);
-          });
-        });
+          throw (await res.text()) || res.statusText;
+        } catch (e) {
+          throw e;
+        }
+      };
     });
   }
 
